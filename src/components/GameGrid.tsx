6f5d62dcac63036e78277a7b87e1b36aad82a389
alijm0806 <alijm1104@gmail.com>
2023-11-24 21:06:16 -0600

    Create a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import { SimpleGrid, Text } from '@chakra-ui/react';
-
-import useGames from '../hooks/useGames';
-import GameCard from './GameCard';
-import GameCardSkeleton from './GameCardSkeleton';
-import GameCardContainer from './GameCardContainer';
-
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardContainer";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6]
+  const { data, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding={10} spacing={10}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
         {isLoading &&
-          skeletons.map(skeleton =>
+          skeletons.map((skeleton) => (
             <GameCardContainer>
               <GameCardSkeleton key={skeleton} />
-            </GameCardContainer>)}
-        {games.map(game =>
+            </GameCardContainer>
+          ))}
+        {data.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
-          </GameCardContainer>)}
+          </GameCardContainer>
+        ))}
       </SimpleGrid>
     </>
-  )
-}
-
-export default GameGrid
+  );
+};
+export default GameGrid;
